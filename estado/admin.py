fe46764f57046cd92cd433baeeb55d571e88151d
PabloA-Figueroa <apfigueroa3@utpl.edu.ec>
2023-12-20 09:44:48 -0500

    Cambio api estado

--- estado/admin.py
+++ estado/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from estado.models import Estado
 @admin.register(Estado)
 class EstadoAdmin(admin.ModelAdmin):
-    list_display = ('precioVentaPorUnidad', 'cantidadProyectada', 'ingresosAdicionales', 'costoPorUnidad', 'gastosOperativos', 'gastosMarketing', 'gastosDesarrollo', 'tasaCrecimiento', 'duracionProyeccion', 'gastosAdicionales', 'ingresos', 'gastos', 'proyecciones', 'beneficios')
-    search_fields = ['usuario']
+    list_display = ['fecha']
+    search_fields = ['fecha']
     #filter_horizontal = ['temas']
 # Register your models here.
--- estado/models.py
+++ estado/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from users.models import User
 # Crear un modelos de estado que tenga 6 campos numericos
 class Estado(models.Model):
+    fecha = models.DateField()
     precioVentaPorUnidad = models.IntegerField()
     cantidadProyectada = models.IntegerField()
     ingresosAdicionales = models.IntegerField()
@@ -9,13 +10,10 @@ class Estado(models.Model):
     gastosOperativos = models.IntegerField()
     gastosMarketing = models.IntegerField()
     gastosDesarrollo = models.IntegerField()
-    tasaCrecimiento = models.IntegerField()
-    duracionProyeccion = models.IntegerField()
     # Gastos adicionales es una variable entera y pueda ser nula
     gastosAdicionales = models.IntegerField(null=True, blank=True)
     ingresos = models.IntegerField()
     gastos = models.IntegerField()
-    proyecciones = models.IntegerField()
     beneficios = models.IntegerField()
     usuario = models.ManyToManyField(User,blank=True)
     # Asignar a este modelo el id de usuario
